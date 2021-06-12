b92040ab5e1e961900908fd2a7558885d107607a
itsstranger <70693576+itsstranger@users.noreply.github.com>
2021-06-12 21:09:23 +0530

    5

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
                     "User Email: $visitor_email.\n".
                     "user message: $message.\n";
 
-    $to ="yaah.education@gmail.com";
+    $to ="newdangerhacker@gmail.com";
     $headers = "From: $email_from \r\n";
     $headers .= "Reply-To: $visitor_email \r\n";
     mail($to,$email_subject,$email_body,$headers);
