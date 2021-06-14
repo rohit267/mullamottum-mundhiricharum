7ca9c0357cd1126964ef0ae80221aab0f5899326
Rohit Mahto <rohitmahto267@gmaill.com>
2021-06-14 19:06:34 +0530

    fix undefined changes

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 
 
-  require 'vendor/phpmailer/phpmailer/src/Exception.php';
-  require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
-  require 'vendor/phpmailer/phpmailer/src/SMTP.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
   // Include autoload.php file
   require 'vendor/autoload.php';
@@ -32,22 +31,26 @@
       // Email ID from which you want to send the email
       $mail->setFrom('jarvisai.ironmanmark@gmail.com');
       // Recipient Email ID where you want to receive emails
-      $mail->addAddress('itsstrangerman@gmail.com');
+      $mail->addAddress($email);
 
       $mail->isHTML(true);
-      $mail->Subject = 'Form Submission';
+      $mail->Subject = $subject;
       $mail->Body = "<h3>Name : $name <br>Email : $email <br>Message : $message</h3>";
 
-      $mail->send();
-      $output = '<div class="alert alert-success">
-                  <h5>Thankyou! for contacting us, We will get back to you soon!</h5>
-                </div>';
+      if($mail->send()){
+        echo '<div class="alert alert-success">
+        <h5>Thankyou! for contacting us, We will get back to you soon!</h5>
+      </div>';
+      }
+     
+      
     } catch (Exception $e) {
       $output = '<div class="alert alert-danger">
                   <h5>' . $e->getMessage() . '</h5>
                 </div>';
+      echo $output;
     }
   }
-  header("Location: c.html");
+  
 ?>
 
